f999af4c85723870b14f24134f032cefe0b3d3e9
Kasun Herath <kasun@thinkcube.com>
2012-02-07 21:50:44 +0530

    Port parameter added to connection.

--- beesql/core.py
+++ beesql/core.py
@@ -5,16 +5,26 @@
 # Author: Kasun Herath <kasunh01@gmail.com>
 # Source: https://github.com/kasun/beesql
 
-def connection(engine='mysql', username=None, password=None, host='localhost', db=None, unix_socket=None):
+import beesql
+
+def connection(engine='mysql', username=None, password=None, host='localhost', port=3306, db=None, unix_socket=None):
     ''' 
-    Create and return a connection to a Database.
+    Create and return a connection to a Database using specified engine.
     Arguments:
         engine: Database to use; Default to mysql. 
         username: Username used to connect to Database; Not used with sqlite.
         password: Password used to connect to Database; Not used with sqlite.
-        host: Host of Database; Default to localhost, Not used with sqlite
+        host: Host of Database; Default to localhost, Not used with sqlite.
+        port: port to connect to Database; Default to 3306, Not used with sqlite.
         db: Database name; Optional, if engine is sqlite a filename is expected.
         unix_socket: Used to connect to the Database through a unix socket; Optional, not used with sqlite. '''
 
-    pass
+    try:
+        mod = __import__('beesql.backends.%s' % (engine), fromlist=['beesql.backends'])
+        connection = getattr(mod, '%sConnection' % (engine.capitalize()))(username, password, host, port, db, unix_socket)
+        return connection
+    except ImportError:
+        raise beesql.BeeSQLError('Invalid engine: %s' % (engine))
+    except AttributeError:
+        raise beesql.BeeSQLError('Invalid engine: %s' % (engine))
 
