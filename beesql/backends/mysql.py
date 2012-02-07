4fc73ad0fa2695fb65a4d797f6eccc98c31ec592
Kasun Herath <kasun@thinkcube.com>
2012-02-07 21:59:35 +0530

    Mysql Connection init implemented.

--- beesql/backends/mysql.py
+++ beesql/backends/mysql.py
@@ -2,13 +2,23 @@
 
 ''' Contains Mysql Database engine. '''
 
+# Author: Kasun Herath <kasunh01@gmail.com>
+# Source: https://github.com/kasun/beesql
+
+import pymysql
+
 from base import BeeSQLBaseConnection
 from beesql import BeeSQLError
 
 class MysqlConnection(BeeSQLBaseConnection):
     ''' MySQL Database Connection. '''
-    def __init__(self, username, password, host='localhost', db=None, unix_socket=None):
-        if (not username or not password):
+    def __init__(self, username, password, host='localhost', port=3306, db=None, unix_socket=None):
+        if (not username or password is None):
             raise BeeSQLError('Engine mysql requires username and password')
-
-
+        if not unix_socket:
+            self.db_connection = pymysql.connect(user=username, passwd=password, host=host, port=port)
+        else:
+            self.db_connection = pymysql.connect(user=username, passwd=password, unix_socket=unix_socket)
+        self.cursor = self.db_connection.cursor()
+        if db and db != '':
+            self.cursor.execute('use %s' % (db))
