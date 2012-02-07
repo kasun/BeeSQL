c9610224788ace49668374174f2a025efe1ff947
Kasun Herath <kasun@thinkcube.com>
2012-02-07 21:59:16 +0530

    Doc header added.

--- beesql/backends/base.py
+++ beesql/backends/base.py
@@ -2,6 +2,9 @@
 
 ''' Contains Base Database Connection. '''
 
+# Author: Kasun Herath <kasunh01@gmail.com>
+# Source: https://github.com/kasun/beesql
+
 class BeeSQLBaseConnection(object):
     ''' Base Abstract Database Connection. '''
     pass
