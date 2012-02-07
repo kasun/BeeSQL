c1c21b085a17a7be50384e2349016760d10fa4f3
Kasun Herath <kasun@thinkcube.com>
2012-02-07 15:37:34 +0530

    Docstring added in beesql.

--- beesql/__init__.py
+++ beesql/__init__.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
-''' BeeSQL: Python SQL wrapper. '''
+''' BeeSQL: Python SQL wrapper. 
+        
+        Goals
+            1. Hide Boring Repetitive Steps in Python Database API.
+            2. Allow for mapping between python datastructures and sql. '''
+
+# Author: Kasun Herath <kasunh01@gmail.com>
+# Source: https://github.com/kasun/beesql
 
 
