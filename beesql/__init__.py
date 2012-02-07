6e911bf7b7826ccaa85fb27f4ccd95f6e162d995
Kasun Herath <kasun@thinkcube.com>
2012-02-07 16:56:26 +0530

    Skeleton connection method added.

--- beesql/__init__.py
+++ beesql/__init__.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-''' BeeSQL: Python SQL wrapper. 
+'''
+BeeSQL: Python SQL wrapper. 
         
-        Goals
-            1. Hide Boring Repetitive Steps in Python Database API.
-            2. Allow for mapping between python datastructures and sql. '''
+Goals
+    1. Hide Boring Repetitive Steps in Python Database API.
+    2. Allow for mapping between python datastructures and sql. '''
 
 # Author: Kasun Herath <kasunh01@gmail.com>
 # Source: https://github.com/kasun/beesql
 
-
+from core import connection
---
+++ beesql/core.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+''' BeeSQL core. '''
+
+# Author: Kasun Herath <kasunh01@gmail.com>
+# Source: https://github.com/kasun/beesql
+
+def connection(engine='mysql', username=None, password=None, host='localhost', db=None, unix_socket=None):
+    ''' 
+    Create and return a connection to a Database.
+    Arguments:
+        engine: Database to use; Default to mysql. 
+        username: Username used to connect to Database; Not used with sqlite.
+        password: Password used to connect to Database; Not used with sqlite.
+        host: Host of Database; Default to localhost.
+        db: Database name; Optional, if engine is sqlite a filename is expected.
+        unix_socket: Used to connect to the Database through a unix socket; Optional, not usable in sqlite. '''
+
+    pass
+
