24d93027f5aea0dd92dba6ea75db6147c8102c5a
Kasun Herath <kasun@thinkcube.com>
2012-02-12 23:59:49 +0530

    Added use, drop test cases.

--- tests/test_mysql.py
+++ tests/test_mysql.py
@@ -23,6 +23,22 @@ class TestMysqlConnection(unittest.TestCase):
         self.assertEqual(self.db.run_query.call_args[0][0].lower(), "INSERT INTO beesql_version (version, name) VALUES (%s, %s)".lower())
         self.assertEqual(self.db.run_query.call_args[0][1], ('0.1', 'Kasun Herath'))
 
+    def test_use(self):
+        ''' use method should generate valid sql. '''
+        self.db.run_query = mock.Mock()
+        self.db.use('beesql_version')
+        self.assertEqual(self.db.run_query.call_args[0][0].lower(), "USE beesql_version".lower())
+
+    def test_drop(self):
+        ''' Drop method should generate valid sql for both if_exists=False and if_exists=True. '''
+        self.db.run_query = mock.Mock()
+        self.db.drop('beesql_version')
+        self.assertEqual(self.db.run_query.call_args[0][0].lower(), "DROP DATABASE beesql_version".lower())
+        self.db.drop('beesql_version', if_exists=True)
+        self.assertEqual(self.db.run_query.call_args[0][0].lower(), "DROP DATABASE IF EXISTS beesql_version".lower())
+        self.db.drop('beesql_version', if_exists=False)
+        self.assertEqual(self.db.run_query.call_args[0][0].lower(), "DROP DATABASE beesql_version".lower())
+
     def tearDown(self):
         self.db.close()
 
